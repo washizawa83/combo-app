7fe39a656b71ebb5304aa6a36ba0a3fa0a01d3f8
washizawa haruya (JSL) <129074090+washizawa83@users.noreply.github.com>
2025-02-11 16:05:14 +0900

    ログイン機能実装 (#2)

--- src/app/components/layouts/Header.tsx
+++ src/app/components/layouts/Header.tsx
@@ -1,13 +1,25 @@
+import { getCurrentAuthUser, logout } from '@/app/service/auth'
+import Link from 'next/link'
 import { PageLayout } from '../PageLayout'
 
-export const Header = () => {
+export const Header = async () => {
+  const currentUser = await getCurrentAuthUser()
+
   return (
     <header className="bg-variant h-16 w-full">
       <PageLayout>
         <div className="flex items-center justify-between h-full">
           <div className="font-rubikOne text-4xl">Combo</div>
           <ul>
-            <li className="font-rubikOne text-lg">Login</li>
+            {currentUser ? (
+              <button className="font-rubikOne text-lg" onClick={logout}>
+                Logout
+              </button>
+            ) : (
+              <Link href={'/pages/login'}>
+                <li className="font-rubikOne text-lg">Login</li>
+              </Link>
+            )}
           </ul>
         </div>
       </PageLayout>
---
+++ src/app/service/auth.ts
@@ -0,0 +1,58 @@
+'use server'
+
+import { PrismaClient } from "@prisma/client/extension"
+import { createClient } from "../utils/supabase/server"
+import { redirect } from 'next/navigation'
+
+export const getCurrentAuthUser = async () => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await (await supabase).auth.getUser()
+  return user
+}
+
+export const createUser = async () => {
+    const prisma = new PrismaClient()
+    const newUser = await prisma.user.create({
+        data: {
+            name: 'test'
+        }
+    })
+    return newUser
+}
+
+export const login = async (provider: 'google') => {
+  const supabase = createClient()
+  const { data, error } = await (
+    await supabase
+  ).auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: `http://localhost:3000/auth/callback`,
+      queryParams: {
+        access_type: 'offline',
+        prompt: 'consent',
+      },
+    },
+  })
+
+  if (error) {
+    redirect('/pages/error')
+  }
+
+  if (data.url) {
+    redirect(data.url)
+  }
+}
+
+export const logout = async () => {
+  const supabase = createClient()
+  const { error } = await (await supabase).auth.signOut()
+
+  if (error) {
+    redirect('/error')
+  }
+
+  redirect('http://localhost:3000/')
+}
